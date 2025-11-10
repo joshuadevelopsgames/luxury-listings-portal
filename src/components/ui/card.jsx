b77a5f6142cc726cd7ee88aa7dd6f54c7ef29e3b
jaygamedeveloperboy <jaygamedeveloperboy@users.noreply.github.com>
2025-11-10 13:21:17 -0800

    Set consistent card padding site-wide with pb-3 on CardHeader

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -12,7 +12,7 @@ Card.displayName = "Card";
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={`flex flex-col space-y-1.5 p-6 ${className || ''}`}
+    className={`flex flex-col space-y-1.5 p-6 pb-3 ${className || ''}`}
     {...props}
   />
 ));
